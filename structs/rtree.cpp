a2aa61656c422456f3fefe8274e96410d0046507
jeanpaulHT <jean.huby@utec.edu.pe>
2022-05-28 21:05:33 -0500

    rtree stuff added, make fuctional later

--- structs/rtree.cpp
+++ structs/rtree.cpp
@@ -1,7 +1,28 @@
 #include "rtree.h"
 
+Rtree::Rtree(Figure f):myFigure(f){
+    
+}
 
 
+Rtree* Rtree::search(const Figure& f){
+    //TODO
+    return {};
+}
+
+void Rtree::split(){
+    
+}
 
 
+bool Rtree::insert(Figure f){
+    Rtree* cur = search(f);
+    
+    if(cur->cur_figs >= ORDER){
+        //split()
+        return true;
+    }
+
+    this->node[cur_figs++] = &Rtree{f};
+}
 
