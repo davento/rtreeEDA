5a05f596c9e5aa820ffe4c43b9f6011fe09d4818
jeanpaulHT <jean.huby@utec.edu.pe>
2022-06-17 22:53:59 -0500

    Bound

--- structs/Bound.cpp
+++ structs/Bound.cpp
@@ -1,11 +1,51 @@
 #include "Bound.h"
 
 
-
-Bound::Bound(): topLeft(INF,INF), bottomRight(INF,INF){
+Bound::Bound(): topLeft(INF,INF), bottomRight(-INF,-INF){
 }
 
 Bound::Bound(const Point& p1, const Point& p2): topLeft(p1), bottomRight(p2){
 
 } 
 
+
+double MBR::perimeter() const {
+    return (abs(topLeft.x - bottomRight.x) +
+        abs(topLeft.y - bottomRight.y ));
+}
+
+double MBR::area() const {
+    return (abs(topLeft.x - bottomRight.x) *
+        abs(topLeft.y - bottomRight.y ));
+}
+
+double MBR::metric() const{
+    return this->perimeter();
+}
+
+void MBR::merge(MBR* m){
+
+    this->topLeft = Point::createMinPoint(this->topLeft,
+    m->topLeft );
+    this->bottomRight = Point::createMaxPoint(this->bottomRight, 
+    m->bottomRight );   
+}
+
+void  MBR::merge(const Point& p){
+    this->topLeft = Point::createMinPoint(this->topLeft,
+                    p);
+    this->bottomRight = Point::createMaxPoint(this->bottomRight,
+                        p);
+}
+
+void MBR::draw(SDL_Renderer* renderer) const {
+
+    if(topLeft.x == INF && topLeft.y == INF)
+        return;
+    const int &lx = topLeft.x, &ty = topLeft.y, &rx = bottomRight.x, &by = bottomRight.y;
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+    SDL_RenderDrawLine(renderer, lx, ty, rx , ty);
+    SDL_RenderDrawLine(renderer, lx, ty, lx , by);
+    SDL_RenderDrawLine(renderer, lx, by, rx , by);
+    SDL_RenderDrawLine(renderer, rx, ty, rx , by);
+}
--- structs/Bound.h
+++ structs/Bound.h
@@ -3,24 +3,48 @@
 
 #include "Point.h"
 
+
+
 class Bound{
 
-    constexpr int INF = 40000;
 
     public:
+        static constexpr int INF = 40000;
+
         Bound();
         Bound(const Point&, const Point&);
         Bound(const Bound&) = default;
+
+
         virtual double perimeter() const = 0;
         virtual double area() const = 0;
         virtual double metric() const = 0;
-        virtual void merge(Bound*) = 0;
+        // virtual void merge(Bound*) = 0;
+
         virtual void merge(const Point&) = 0;
         //TODO: Agregar color luego
         virtual void draw(SDL_Renderer* renderer) const = 0;
 
     protected:
         Point topLeft, bottomRight;
+
+};
+
+class MBR: public Bound{
+
+    public:
+        MBR();
+        MBR(const Point&, const Point&);
+        MBR(const Bound&);
+
+        virtual double perimeter() const override;
+        virtual double area() const override;
+        virtual double metric() const override;
+        //virtual
+        void merge(MBR*);
+        virtual void merge(const Point&) override;
+        //TODO: Agregar color luego
+        virtual void draw(SDL_Renderer* renderer) const override;
 
 };
 
