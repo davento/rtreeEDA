d5b070b2da06538794a9882a279cf6df79368fdc
jeanpaulHT <jean.huby@utec.edu.pe>
2022-06-17 20:57:20 -0500

    Figure methods added

---
+++ structs/Figure.cpp
@@ -0,0 +1,42 @@
+#include "Figure.h"
+
+Figure::Figure(Bound* mb): myBound(mb){
+
+}
+
+Bound* Figure::getBound(){
+    return myBound;
+}
+
+
+//wtf does this supposed to do?
+bool Figure::addPoint(const Point&p){
+
+    if (points.size() == 0 or
+        !points.front().insideRadious(p,4))
+    {
+        points.push_back(p);
+        myBound->merge(p);
+        return true;
+    }
+    else(points.front().insideRadious(p,4)){
+        points.push_back(points.front());
+        return false;
+    }
+}
+
+void Figure::draw(SDL_Renderer* renderer) const{
+
+    if(points.size() == 0)
+        return;
+    for(const auto& point: points)     
+        point.draw(renderer);
+    if(points.size() <= 1)
+            return;
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    
+    for(size_t i = 0; i != points.size() - 1; i++){
+        SDL_RenderDrawLine(renderer, points[i].x, points[i].y, points[i+1].x, points[i+1].y);
+    }
+    myBound->draw(renderer);
+}
