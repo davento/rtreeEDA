bf6dec6283b209cb9fd41771fba2797343287417
jeanpaulHT <jean.huby@utec.edu.pe>
2022-06-27 00:48:01 -0500

    A

--- Display.cpp
+++ Display.cpp
@@ -14,22 +14,26 @@ bool Display::initialize(double dim){
         SDL_Log("No se pudo inicializar SDL: %s", SDL_GetError());
         return false;
     }
-    window = SDL_CreateWindow("Rtree", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, dim, dim,0);
+    window = SDL_CreateWindow("Rtree", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600,0);
     if(!window){
         SDL_Log("Falla en la creación de la ventana: %s", SDL_GetError());
     }
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     isRunning = true;
     
     r = new Rtree<MBC,3>;
 
     return true;
+
 }
 
 void Display::runLoop(){
     while(isRunning){
+        printf("input\n");
         processInputs();
+        printf("Display\n");
         updateDisplay();
+        printf("Output\n");
         generateOutput();
     }
 }
@@ -42,40 +46,44 @@ void Display::shutdown(){
 
 void Display::processInputs(){
     SDL_Event event;
-    while(SDL_PollEvent(&event)){
-        switch(event.type){
-            case SDL_QUIT:
+    bool quit = false;
+    std::cout<<"A\n";
+
+
+    while (!quit){
+        while(SDL_PollEvent(&event)){
+            if(event.type == SDL_QUIT){
+                quit = true;
                 isRunning = false;
-                break;
-            case SDL_MOUSEBUTTONDOWN:
-                {
-                    int x, y;
-                    
-                    SDL_GetMouseState(&x, &y);
-                    if(event.button.button == SDL_BUTTON_LEFT){
-                       if(!fig.addPoint(Point(x,y))){
-                           r->insert(&fig);
-                           fig.clear();
-                       }
+            }
+            
+            else if(event.type == SDL_MOUSEBUTTONDOWN){
+                int x, y;
+            
+                SDL_GetMouseState(&x, &y);
+                if(event.button.button == SDL_BUTTON_LEFT){
+                    if(!fig.addPoint(Point(x,y))){
+                       r->insert(&fig);
+                       fig.clear();
                     }
-                    if(event.button.button == SDL_BUTTON_RIGHT){
-                        r->remove(Point(x,y));
-                        fig.clear();
-                    }
-                    break;
                 }
-            default:
-                {
-                    int x, y;
-                    //std::cout << "default" << std::endl; 
-                    SDL_GetMouseState(&x, &y);
-                    Point po(x,y);
-                    figures = r->depthFirst(po);
-                    //std::cout << figures.size() << std::endl;
-                    break;
+                if(event.button.button == SDL_BUTTON_RIGHT){
+                    r->remove(Point(x,y));
+                    fig.clear();
                 }
+            }
+            else{
+                int x, y;
+                std::cout << "default" << std::endl; 
+                SDL_GetMouseState(&x, &y);
+                Point po(x,y);
+                figures = r->depthFirst(po);
+                //std::cout << figures.size() << std::endl;
+                quit = true;
+            }
         }
     }
+    
     const Uint8* state = SDL_GetKeyboardState(NULL);
     if(state[SDL_SCANCODE_ESCAPE])
         isRunning = false;
