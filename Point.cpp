607fff1a67e0c3cd9ae9b80ab942fb6c010a523b
Dio12334 <diego.paredes.r@utec.edu.pe>
2022-07-03 17:31:24 -0500

    puntos

---
+++ Point.cpp
@@ -0,0 +1,57 @@
+#include "Point.h"
+
+#include "Point.h"
+#include <algorithm>
+
+Point::Point(double posX, double posY): x(posX), y(posY){
+
+}
+
+bool operator ==(const Point& Left, const Point& right){
+    return (Left.x == right.x) && (Left.y == right.y);
+}
+
+bool operator !=(const Point& left, const Point& right){
+    return  !(left==right);
+}
+
+
+double Point::distance(const Point& from, const Point& to){
+    return std::sqrt(std::pow(from.x - to.x, 2) + std::pow(from.y - to.y, 2));
+}
+
+bool Point::closeEnough(const Point& p1, const Point& p2, const int RADIUS){
+    return Point::distance(p1, p2) < RADIUS * 2;
+}
+
+
+void Point::draw(SDL_Renderer* renderer) const{
+    // SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    // SDL_RenderDrawPoint(renderer, x, y);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    int dim = 1;
+    SDL_Rect dot{
+        static_cast<int>(x - dim),
+        static_cast<int>(y - dim),
+        3*dim,
+        3*dim
+    };
+    SDL_RenderFillRect(renderer, &dot);
+}
+
+Point Point::createMinPoint(const Point& p1, const Point& p2){
+    return Point(
+            std::min(p1.x, p2.x),
+            std::min(p1.y, p2.y)
+    );
+}
+
+
+Point Point::createMaxPoint(const Point& p1, const Point& p2){
+    return Point(
+            std::max(p1.x, p2.x),
+            std::max(p1.y, p2.y)
+    );
+}
+
+
--- Point.h
+++ Point.h
@@ -1,8 +1,26 @@
 #ifndef POINT_H
 #define POINT_H
 
-struct Point{
+#include <SDL2/SDL.h>
+
+struct Point final{
     double x, y;
+
+    Point() = default;
+    explicit Point(double posX, double posY);
+    Point(const Point&) = default;
+    Point& operator =(const Point&) = default;
+    ~Point() = default;
+
+    friend bool operator==(const Point& left, const Point& right);
+    friend bool operator!=(const Point& left, const Point& right);
+    
+    static double distance(const Point& from, const Point& to);
+    static bool closeEnough(const Point& p1, const Point& p2, const int RADIUS);
+    static Point createMinPoint(const Point& left, const Point& right);
+    static Point createMaxPoint(const Point& left, const Point& right);
+
+    void draw(SDL_Renderer* renderer) const;
 };
 
 #endif
