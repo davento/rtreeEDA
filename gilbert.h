c4660435ff7008c732471f34d9cde299670e28ec
jeanpaulHT <jean.huby@utec.edu.pe>
2022-07-11 17:36:16 -0500

    gil

---
+++ gilbert.h
@@ -0,0 +1,31 @@
+#ifndef GILBERT_H
+#define GILBERT_H
+
+
+void rot(int n, int *x, int *y, int rx, int ry) {
+    if (ry == 0) {
+        if (rx == 1) {
+            *x = n-1 - *x;
+            *y = n-1 - *y;
+        }
+
+        //Swap x and y
+        int t  = *x;
+        *x = *y;
+        *y = t;
+    }
+}
+
+
+int xy2d (int n, int x, int y) {
+  int rx, ry, s, d=0;
+    for (s=n/2; s>0; s/=2) {
+        rx = (x & s) > 0;
+        ry = (y & s) > 0;
+        d += s * s * ((3 * rx) ^ ry);
+        rot(n, &x, &y, rx, ry);
+    }
+    return d;
+}
+
+#endif
